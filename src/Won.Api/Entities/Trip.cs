5ea96019a9a165daae69d29c6ca7d1e8ecbf585e
tomlenton <twlenton@gmail.com>
2026-05-27 14:33:39 +0100

    Feat: Added Trip to Entities folder

---
+++ src/Won.Api/Entities/Trip.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Won.Api.Entities
+{
+    public class Trip
+    {
+        public int TripId { get; set; }
+        [Required(ErrorMessage = "Trip Name Is Required")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Start Date Is Required")]
+        public DateTime StartDate { get; set; }
+        [Required(ErrorMessage = "End Date Is Required")]
+        public DateTime EndDate { get; set; }
+        [Required(ErrorMessage = "Location Is Required")]
+        public string Location { get; set; }
+        public string Details { get; set; }
+        [Required(ErrorMessage = "Budget Is Required")]
+        public int Budget { get; set; }
+        [Required(ErrorMessage = "Group Size Is Required")]
+        public int GroupSize { get; set; }
+    }
+}
