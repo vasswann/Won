7f9e3c016df654e3a10ae364d10ea604d715701b
Kati Stark <kati.stark95@gmail.com>
2026-05-27 11:31:41 +0100

    Add GetTripCard Component and a page for each unique tripID

--- src/Won.Web/Won.Web/Models/Trip.cs
+++ src/Won.Web/Won.Web/Models/Trip.cs
@@ -1,6 +1,22 @@
-﻿namespace Won.Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Won.Web.Models
 {
     public class Trip
     {
+        public int TripID { get; set; }
+        [Required(ErrorMessage = "Trip Name Is Required")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Start Date Is Required")]
+        public DateTime StartDate { get; set; }
+        [Required(ErrorMessage = "End Date Is Required")]
+        public DateTime EndDate { get; set; }
+        [Required(ErrorMessage = "Location Is Required")]
+        public string Location { get; set; }
+        public string Details { get; set; }
+        [Required(ErrorMessage = "Budget Is Required")]
+        public int Budget { get; set; }
+        [Required(ErrorMessage = "Group Size Is Required")]
+        public int GroupSize { get; set; }
     }
 }
