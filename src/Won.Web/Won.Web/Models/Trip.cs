6db9cdfdcd01b22c14fa05b70b17e5c6449b21fe
Kati Stark <kati.stark95@gmail.com>
2026-05-27 14:57:53 +0100

    Remove validation from model

--- src/Won.Web/Won.Web/Models/Trip.cs
+++ src/Won.Web/Won.Web/Models/Trip.cs
@@ -5,18 +5,12 @@ namespace Won.Shared.Models
     public class Trip
     {
         public int TripId { get; set; }
-        [Required(ErrorMessage = "Trip Name Is Required")]
         public string Name { get; set; } = String.Empty;
-        [Required(ErrorMessage = "Start Date Is Required")]
         public DateTime StartDate { get; set; }
-        [Required(ErrorMessage = "End Date Is Required")]
         public DateTime EndDate { get; set; }
-        [Required(ErrorMessage = "Location Is Required")]
         public string Location { get; set; } = String.Empty;
         public string Details { get; set; } = String.Empty;
-        [Required(ErrorMessage = "Budget Is Required")]
         public decimal Budget { get; set; }
-        [Required(ErrorMessage = "Group Size Is Required")]
         public int GroupSize { get; set; }
     }
 }
