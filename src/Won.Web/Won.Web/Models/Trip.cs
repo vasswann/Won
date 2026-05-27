70373855cb6f8ae984f395ca5f2329279fdc86d9
Kati Stark <kati.stark95@gmail.com>
2026-05-27 14:55:19 +0100

    Actioned Pull Request Comments

--- src/Won.Web/Won.Web/Models/Trip.cs
+++ src/Won.Web/Won.Web/Models/Trip.cs
@@ -1,21 +1,21 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace Won.Web.Models
+namespace Won.Shared.Models
 {
     public class Trip
     {
-        public int TripID { get; set; }
+        public int TripId { get; set; }
         [Required(ErrorMessage = "Trip Name Is Required")]
-        public string Name { get; set; }
+        public string Name { get; set; } = String.Empty;
         [Required(ErrorMessage = "Start Date Is Required")]
         public DateTime StartDate { get; set; }
         [Required(ErrorMessage = "End Date Is Required")]
         public DateTime EndDate { get; set; }
         [Required(ErrorMessage = "Location Is Required")]
-        public string Location { get; set; }
-        public string Details { get; set; }
+        public string Location { get; set; } = String.Empty;
+        public string Details { get; set; } = String.Empty;
         [Required(ErrorMessage = "Budget Is Required")]
-        public int Budget { get; set; }
+        public decimal Budget { get; set; }
         [Required(ErrorMessage = "Group Size Is Required")]
         public int GroupSize { get; set; }
     }
