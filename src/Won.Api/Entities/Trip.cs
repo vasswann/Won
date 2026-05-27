ffc4f5002952ac06a8a68d31a8af47f7254d4b90
Tom Lenton <twlenton@gmail.com>
2026-05-27 15:06:02 +0100

    Initialize string properties and update Budget type
    Initialized string properties and changed Budget type to decimal.

--- src/Won.Api/Entities/Trip.cs
+++ src/Won.Api/Entities/Trip.cs
@@ -5,12 +5,12 @@ namespace Won.Api.Entities
     public class Trip
     {
         public int TripId { get; set; }
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
-        public string Location { get; set; }
-        public string Details { get; set; }
-        public int Budget { get; set; }
+        public string Location { get; set; }  = string.Empty;
+        public string Details { get; set; }  = string.Empty;
+        public decimal Budget { get; set; }
         public int GroupSize { get; set; }
     }
 }
