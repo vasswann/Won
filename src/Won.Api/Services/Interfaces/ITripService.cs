e991469188c8d2dceb7a68db0b16d7835786adf6
tomlenton <twlenton@gmail.com>
2026-05-27 11:48:20 +0100

    feat: added ITripService interface

---
+++ src/Won.Api/Services/Interfaces/ITripService.cs
@@ -0,0 +1,12 @@
+﻿using Won.Shared.Dtos;
+namespace Won.Api.Services.Interfaces
+{
+    public interface ITripService
+    {
+        Task<List<Trip>> GetTripsAsync();
+        Task<Trip?> GetTripByIdAsync(int id);
+        Task<Trip> CreateTripAsync(CreateTripDto dto);
+        Task<Trip?> UpdateTripAsync(int id, UpdateTripDto dto);
+        Task<bool> DeleteTripAsync(int id);
+    }
+}
