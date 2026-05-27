3210ac89235ffd5e0ed009b090af8521c6e645bc
tomlenton <twlenton@gmail.com>
2026-05-27 11:39:55 +0100

    feat: add CreateTripDto for trip creation

---
+++ src/Won.Shared/Dtos/CreateTripDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Won.Shared.Dtos
+{
+    public class CreateTripDto
+    {
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Location { get; set; }
+        public string Details { get; set; }
+        public int Budget { get; set; }
+        public int GroupSize { get; set; }
+    }
+}
