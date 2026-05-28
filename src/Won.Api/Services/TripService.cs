d408b2fed39a9713270a3960d62006f46818f1c8
tomlenton <twlenton@gmail.com>
2026-05-28 10:52:13 +0100

    Feat: Added CreateTrip to the TripService

--- src/Won.Api/Services/TripService.cs
+++ src/Won.Api/Services/TripService.cs
@@ -1,6 +1,7 @@
 ﻿using Won.Api.Entities;
 using Won.Api.Repositories.Interfaces;
 using Won.Api.Services.Interfaces;
+using Won.Shared.Dtos;
 
 namespace Won.Api.Services
 {
@@ -19,6 +20,21 @@ namespace Won.Api.Services
         public async Task<Trip?> GetTripByIdAsync(int id)
         {
             return await _tripRepository.GetTripByIdAsync(id);
+        }
+        public async Task<Trip> CreateTripAsync(CreateTripDto tripData)
+        {
+            var trip = new Trip
+            {
+                Name = tripData.Name,
+                StartDate = tripData.StartDate,
+                EndDate = tripData.EndDate,
+                Location = tripData.Location,
+                Details = tripData.Details,
+                Budget = tripData.Budget,
+                GroupSize = tripData.GroupSize
+            };
+
+            return await _tripRepository.CreateTripAsync(trip);
         }
     }
 }
