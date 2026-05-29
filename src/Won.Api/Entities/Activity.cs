a9bbc6fa8fb184e39fb98388cfd3c70ed838e1d8
vasswann <swannxxx@yahoo.co.uk>
2026-05-29 17:00:00 +0100

    feat: migrated activities database

---
+++ src/Won.Api/Entities/Activity.cs
@@ -0,0 +1,16 @@
+﻿namespace Won.Api.Entities;
+
+public class Activity
+{
+    public int ActivityId { get; set; }
+    public int TripId { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public decimal Cost { get; set; }
+    public int WeatherDependency { get; set; }
+    public int EnergyIntensity { get; set; }
+    public int MinimumGroupSize { get; set; }
+    public int MaximumGroupSize { get; set; }
+    public DateTime ActivityDateTime { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public Trip? Trip { get; set; }
+}
