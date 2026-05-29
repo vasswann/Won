3f061207fe6c27980dd34535c14c61b6125aa2c5
tomlenton <twlenton@gmail.com>
2026-05-29 14:23:31 +0100

    Feat: Added TripDto

---
+++ src/Won.Shared/Dtos/TripDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Won.Shared.Dtos
+{
+    public class TripDto
+    {
+        public int TripId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string? Location { get; set; }
+    }
+}
