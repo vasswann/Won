ded81e5f534c237d89448ecdf314cdd5638a2bec
Tom Lenton <twlenton@gmail.com>
2026-05-29 16:28:30 +0100

    Refactor: TripDto by modifying properties
    Removed TripId and Location properties, added Details, Budget, and GroupSize properties.

--- src/Won.Shared/Dtos/TripDto.cs
+++ src/Won.Shared/Dtos/TripDto.cs
@@ -2,10 +2,12 @@
 {
     public class TripDto
     {
-        public int TripId { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public DateTime StartDate { get; set; }
-        public DateTime EndDate { get; set; }
-        public string? Location { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public string Location { get; set; } = string.Empty;
+    public string Details { get; set; } = string.Empty;
+    public int Budget { get; set; }
+    public int GroupSize { get; set; }
     }
 }
