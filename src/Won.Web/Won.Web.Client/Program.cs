eb0ea9eccbae8197d9bf97b229316e2acc96e1f2
Zitian Harper <Zitian.harper@icloud.com>
2026-06-01 14:48:51 +0100

    added BaseAddress to won.web.client.program

--- src/Won.Web/Won.Web.Client/Program.cs
+++ src/Won.Web/Won.Web.Client/Program.cs
@@ -1,5 +1,15 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
+using System.Net.Http;
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+await builder.Build().RunAsync();
+
+builder.Services.AddScoped(sp =>
+    new HttpClient
+    {
+        BaseAddress = new Uri("https://localhost:7002")
+    });
 
 await builder.Build().RunAsync();
