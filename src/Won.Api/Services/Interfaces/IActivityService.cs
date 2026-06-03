0acfe3c04cdbfa1248a3da58cae414f45c2e1180
tomlenton <twlenton@gmail.com>
2026-06-03 11:59:11 +0100

    Feat: Added IActivityService

---
+++ src/Won.Api/Services/Interfaces/IActivityService.cs
@@ -0,0 +1,13 @@
+﻿using Won.Shared.Dtos;
+
+namespace Won.Api.Services.Interfaces
+{
+    public interface IActivityService
+    {
+        Task<List<ActivityDto>> GetActivitiesByTripIdAsync(int tripId);
+        Task<ActivityDto?> GetActivityByIdAsync(int activityId);
+        Task<ActivityDto> CreateActivityAsync(CreateActivityDto activityData);
+        Task<ActivityDto?> UpdateActivityAsync(int activityId, UpdateActivityDto updatedActivityData);
+        Task<bool> DeleteActivityAsync(int activityId);
+    }
+}
