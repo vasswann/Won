935d02c1bfbb0c5fc3d14836cfb55378e38b02ed
Kati Stark <kati.stark95@gmail.com>
2026-06-03 15:10:39 +0100

    Updated with TripDto and reformat update.razor

---
+++ src/Won.Shared/Dtos/TripDto.cs
@@ -0,0 +1,14 @@
+﻿namespace Won.Shared.Dtos
+{
+    public class TripDto
+    {
+        public int TripId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Location { get; set; } = string.Empty;
+        public string Details { get; set; } = string.Empty;
+        public decimal Budget { get; set; }
+        public int GroupSize { get; set; }
+    }
+}
