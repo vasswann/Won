b07ec9901b90f5a2a864c167f2f2924589d68664
tomlenton <twlenton@gmail.com>
2026-06-03 12:01:15 +0100

    Feat: Added UpdateActivityDto

---
+++ src/Won.Shared/Dtos/UpdateActivityDto.cs
@@ -0,0 +1,14 @@
+﻿namespace Won.Shared.Dtos
+{
+    public class UpdateActivityDto
+    {
+        public int TripId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public decimal Cost { get; set; }
+        public int WeatherDependency { get; set; }
+        public int EnergyIntensity { get; set; }
+        public int MinimumGroupSize { get; set; }
+        public int MaximumGroupSize { get; set; }
+        public DateTime ActivityDateTime { get; set; }
+    }
+}
